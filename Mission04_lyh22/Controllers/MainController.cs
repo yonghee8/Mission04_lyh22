da673bc1acd000c30f1610696ffb7d3557e98f76
yonghee8 <114109565+yonghee8@users.noreply.github.com>
2023-02-06 15:17:07 -0700

    Mission 5

---
+++ Mission04_lyh22/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_lyh22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_lyh22.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_lyh22/Models/CalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_lyh22.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int A { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int P { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int Q { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int M { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int F { get; set; }
+
+        [Range(0, 100, ErrorMessage = "The grade must be between 0 and 100.")]
+        public int I { get; set; }
+    }
+
+}
